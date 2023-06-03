3fcb857ef3f5ed35d8f79d3239862f9ae3f42518
Abdessadiq <abdoubhm2018@gmail.com>
2023-06-03 21:23:25 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { AngularFireModule } from '@angular/fire/compat';
 
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAs_s_N7X6bAgnOnp1ROlDFlFt7ItaMdY4",
+  authDomain: "ionic-5526b.firebaseapp.com",
+  projectId: "ionic-5526b",
+  storageBucket: "ionic-5526b.appspot.com",
+  messagingSenderId: "951013538912",
+  appId: "1:951013538912:web:990a27016bd60463869705",
+  measurementId: "G-NR3ML304ET"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+// export const  firebaseConfig ={
+//   apiKey:'',
+//   authDomain:'*****.firebaseapp.com',
+//   databaseURL:'https//************.forebaseio.com',
+//   projectId:'',
+//   storageBucket:'',
+//   messagingSenderId:'',
+//   appId:'',
+//
+// };
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+import { AngularFirestore, DocumentSnapshot } from '@angular/fire/compat/firestore';
+
+import { Observable} from 'rxjs';
+
+import { map, first } from 'rxjs';
+
+interface Item {
+  text: string;
+}
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
-})
-export class Tab1Page {
 
-  constructor() {}
+})
+
+export class Tab1Page {
+  itemDoc: Observable<DocumentSnapshot<Item>>;
+  items: Observable<Item[]>;
+  constructor(private firestore: AngularFirestore) {
+    this.itemDoc = this.firestore.doc<Item>('collectionName/documentId').snapshotChanges()
+      .pipe(map(snapshot => snapshot.payload),first()
+      );
+
+    this.items = this.itemDoc.pipe(map(snapshot => [snapshot.data() as Item]));
+  }
 
 }
